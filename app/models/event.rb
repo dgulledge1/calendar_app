71de3a4661d8e0cb3e14602a09fc596bf7acfc8c
Derek Gulledge <dgulledge@neuone.com>
2013-06-07 17:00:04 -0500

    Changed the landing page some

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,13 @@ class Event < ActiveRecord::Base
   
   attr_accessible :end_at, :name, :start_at, :recurrence
   has_event_calendar
+  
+  def schedule
+    new_schedule = Schedule.new(start_at)
+    new_schedule.add_reccurence_rule(Rule.daily)
+    new_schedule.add_recurrence_rule(Rule.terminating?(:end_at))    
+    new_schedule.add_exception_time(:saturday)
+    new_schedule.add_exception_time(:sunday)
+    new_schedule
+  end
 end
